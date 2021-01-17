25585679d3e9adad8b5a440c2c86464d7452cae6
Teo Zosa <teofilo@sonosim.com>
2021-01-16 21:58:06 -0800

    :white_check_mark: Add `test_version_option()`
    Bringing test coverage back up to 100%.
    
    Signed-off-by: Teo Zosa <teofilo@sonosim.com>

--- {{cookiecutter.project_slug}}/tests/test_main.py
+++ {{cookiecutter.project_slug}}/tests/test_main.py
@@ -10,3 +10,8 @@ def test_main_succeeds() -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(app)
     assert result.exit_code == 0
+
+def test_version_option() -> None:
+    """It returns the package version."""
+    result = runner.invoke(app, ["--version"])
+    assert "0.0.0" in result.output
