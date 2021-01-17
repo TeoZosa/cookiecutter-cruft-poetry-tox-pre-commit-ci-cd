ca7d431655464bc355b674b26953c42858a54d7b
Teo Zosa <teofilo@sonosim.com>
2021-01-17 10:58:45 -0800

    :white_check_mark: Add `test_version_determination_logic()`

--- {{cookiecutter.project_slug}}/tests/test_main.py
+++ {{cookiecutter.project_slug}}/tests/test_main.py
@@ -1,7 +1,7 @@
 """Test cases for the __main__ module."""
 from typer.testing import CliRunner
 
-from {{cookiecutter.package_name}}.__main__ import app
+from {{cookiecutter.package_name}}.__main__ import __version__, app
 
 from . import utils
 
@@ -18,3 +18,7 @@ def test_version_option() -> None:
     """It returns the package version."""
     result = runner.invoke(app, ["--version"])
     assert utils.get_version_number() in result.output
+
+def test_version_determination_logic() -> None:
+    """Validates the installed package version against the project-specified version."""
+    assert __version__ == utils.get_version_number()
