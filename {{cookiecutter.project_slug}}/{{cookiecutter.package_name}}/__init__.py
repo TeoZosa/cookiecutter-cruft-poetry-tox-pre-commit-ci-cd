24053d11cf317fbef3d7f7ed7d06212f917b01f2
Teo Zosa <teofilo@sonosim.com>
2021-05-22 16:13:47 -0700

    :sparkles: Add automated Sentry exception monitoring

--- {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,4 +1,11 @@
 """{{cookiecutter.friendly_name}}."""
 from typing import List
 
+import sentry_sdk
+from dotenv import find_dotenv, load_dotenv
+
 __all__: List[str] = []
+
+load_dotenv(find_dotenv())
+# Note: if DSN isn't defined, will silently not transmit telemetry
+sentry_sdk.init()  # pylint: disable=abstract-class-instantiated
