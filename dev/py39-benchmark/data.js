8a6bb3154bcb8b4e5a742ee4a46eeb665c1aaecf
github-action-benchmark <github@users.noreply.github.com>
2021-06-08 22:31:54 +0000

    add Benchmark (pytest) benchmark result for faafc60d62dff61a6da2efd0426003aee3764cf4

--- dev/py39-benchmark/data.js
+++ dev/py39-benchmark/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1623179469516,
+  "lastUpdate": 1623191514067,
   "repoUrl": "https://github.com/TeoZosa/cookiecutter-cruft-poetry-tox-pre-commit-ci-cd",
   "entries": {
     "Benchmark": [
@@ -62,6 +62,68 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 3.424186086275462e-8",
             "extra": "mean: 197.60815909448144 nsec\nrounds: 54055"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "teofilo@sonosim.com",
+            "name": "Teo Zosa"
+          },
+          "committer": {
+            "email": "teofilo@sonosim.com",
+            "name": "Teo Zosa"
+          },
+          "id": "faafc60d62dff61a6da2efd0426003aee3764cf4",
+          "message": ":green_heart: Fix `yamllint` `(line-length)` errors\n\n```\n.github/workflows/release.yml\n  7:81      error    line too long (81 > 80 characters)  (line-length)\n````",
+          "timestamp": "2021-06-08T22:26:38Z",
+          "tree_id": "270e1514628e6688a649c67bd51095ebcbd3d4c9"
+        },
+        "date": 1623191512491,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_icontract_benchmark.py::TestDesignByContractPerformance::test_baseline[simulated computation]",
+            "value": 15695.00679274682,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00002689543198590891",
+            "extra": "mean: 63.71453120123101 usec\nrounds: 12756"
+          },
+          {
+            "name": "tests/test_icontract_benchmark.py::TestDesignByContractPerformance::test_baseline[no simulated computation]",
+            "value": 3806893.5010769023,
+            "unit": "iter/sec",
+            "range": "stddev: 8.177294782390034e-7",
+            "extra": "mean: 262.68136991946636 nsec\nrounds: 196079"
+          },
+          {
+            "name": "tests/test_icontract_benchmark.py::TestDesignByContractPerformance::test_icontract_contract_on[simulated computation]",
+            "value": 12941.071928164672,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00001526290966061231",
+            "extra": "mean: 77.273351508357 usec\nrounds: 13718"
+          },
+          {
+            "name": "tests/test_icontract_benchmark.py::TestDesignByContractPerformance::test_icontract_contract_on[no simulated computation]",
+            "value": 120860.70804496614,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00000762818566961395",
+            "extra": "mean: 8.273987602554428 usec\nrounds: 22989"
+          },
+          {
+            "name": "tests/test_icontract_benchmark.py::TestDesignByContractPerformance::test_icontract_contract_off[simulated computation]",
+            "value": 15627.60896144282,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00010199639513167187",
+            "extra": "mean: 63.9893154779626 usec\nrounds: 7113"
+          },
+          {
+            "name": "tests/test_icontract_benchmark.py::TestDesignByContractPerformance::test_icontract_contract_off[no simulated computation]",
+            "value": 3783464.706524407,
+            "unit": "iter/sec",
+            "range": "stddev: 2.075099054190815e-7",
+            "extra": "mean: 264.30800273504116 nsec\nrounds: 185220"
           }
         ]
       }
