4e6fc63a1f3bcb8ff2e8993315b9c3fcad0d5115
Teo Zosa <erinzosa@ucla.edu>
2021-09-02 16:53:17 -0700

    :boom: Increase conditionally included/excludes files/configs granularity

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,6 +6,14 @@ REMOVE_PATHS = [
     "{% if cookiecutter.adr_documentation_support == 'no' %} docs/source/akm {% endif %}",
     "{% if cookiecutter.adr_documentation_support == 'no' %} docs/source/adr.md {% endif %}",
     "{% if cookiecutter.adr_documentation_support == 'no' %} .github/workflows/publish_log4brains.yml {% endif %}",
+    "{% if cookiecutter.jupyter_notebook_project == 'yes' %} {{cookiecutter.package_name}}/main.py {% endif %}",
+    "{% if cookiecutter.jupyter_notebook_project == 'yes' %} tests/test_main.py {% endif %}",
+    "{% if cookiecutter.jupyter_notebook_project == 'yes' %} {{cookiecutter.package_name}}/entrypoint {% endif %}",
+    "{% if cookiecutter.jupyter_notebook_project == 'yes' %} build.py {% endif %}",
+    "{% if cookiecutter.jupyter_notebook_project == 'no' %} {{cookiecutter.package_name}}/example.ipynb {% endif %}",
+    # i.e., `hybrid` or `no`
+    "{% if cookiecutter.jupyter_notebook_project == 'yes' %} .gitattributes {% endif %}",
+    "{% if cookiecutter.jupyter_notebook_project == 'yes' %} .github/workflows/publish_benchmarks.yml {% endif %}",
 ]
 
 for path in REMOVE_PATHS:
