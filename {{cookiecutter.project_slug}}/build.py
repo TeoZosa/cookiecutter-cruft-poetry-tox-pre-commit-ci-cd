bdf821bb8d14cd59c72a84defb3525ee1333919a
Teo Zosa <erinzosa@ucla.edu>
2021-10-01 12:09:44 -0700

    :recycle: Extract `mypyc` paths arg

--- {{cookiecutter.project_slug}}/build.py
+++ {{cookiecutter.project_slug}}/build.py
@@ -1,3 +1,5 @@
+import pathlib
+
 import mypyc.build
 
 
@@ -5,15 +7,14 @@ def build(setup_kwargs: dict) -> None:
     """
     This function is mandatory in order to build the extensions.
     """
+    paths = [
+        "--disallow-untyped-defs",
+        "--ignore-missing-imports",
+        "{{cookiecutter.package_name}}/__init__.py",
+        "{{cookiecutter.package_name}}/main.py",
+    ]
     setup_kwargs.update(
         {
-            "ext_modules": mypyc.build.mypycify(
-                [
-                    "--disallow-untyped-defs",
-                    "--ignore-missing-imports",
-                    "{{cookiecutter.package_name}}/__init__.py",
-                    "{{cookiecutter.package_name}}/main.py",
-                ]
-            ),
+            "ext_modules": mypyc.build.mypycify(paths),
         }
     )
