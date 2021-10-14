165b97cb7dfa7e83f451342a4995583f731f937a
Teo Zosa <erinzosa@ucla.edu>
2021-10-14 10:39:12 -0700

    :recycle: Extract separate vars: `build_args`, `ext_modules`

--- {{cookiecutter.project_slug}}/build.py
+++ {{cookiecutter.project_slug}}/build.py
@@ -7,12 +7,16 @@ def build(setup_kwargs: dict) -> None:
     """
     This function is mandatory in order to build the extensions.
     """
-    paths = [
+    build_args = [
         "--disallow-untyped-defs",
         "--ignore-missing-imports",
+    ]
+    ext_modules = [
         "{{cookiecutter.package_name}}/__init__.py",
         "{{cookiecutter.package_name}}/main.py",
     ]
+    paths = build_args + ext_modules
+
     setup_kwargs.update(
         {
             "ext_modules": mypyc.build.mypycify(paths),
