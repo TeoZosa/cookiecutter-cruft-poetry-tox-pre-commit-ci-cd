23452e1844b731de60892312ff6decc216d5f165
Teo Zosa <erinzosa@ucla.edu>
2021-10-14 10:45:39 -0700

    :fire: Remove redundant build args
    Already specified in (and overridden by) `mypy` configurations in
    `pyproject.toml`.

--- {{cookiecutter.project_slug}}/build.py
+++ {{cookiecutter.project_slug}}/build.py
@@ -7,15 +7,13 @@ def build(setup_kwargs: dict) -> None:
     """
     This function is mandatory in order to build the extensions.
     """
-    build_args = [
-        "--disallow-untyped-defs",
-        "--ignore-missing-imports",
-    ]
     ext_modules = [
-        "{{cookiecutter.package_name}}/__init__.py",
-        "{{cookiecutter.package_name}}/main.py",
+        str(file)
+        for file in (
+            pathlib.Path(__file__).resolve().parent / "{{cookiecutter.package_name}}"
+        ).rglob("*.py")
     ]
-    paths = build_args + ext_modules
+    paths = ext_modules
 
     setup_kwargs.update(
         {
